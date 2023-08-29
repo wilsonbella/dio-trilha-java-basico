f0818679644301ddfbc6c72e31d987bfc150b79f
bellawilson <wilsonbella880@gmail.com>
2023-08-29 10:12:00 -0300

    feat: meu projeto conta banco

---
+++ src/ContaTerminal.java
@@ -0,0 +1,10 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        System.out.println("chegamos");
+
+    }
+}
